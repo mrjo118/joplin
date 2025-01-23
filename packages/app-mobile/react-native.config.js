0ef0ed9ecdf24139869ccf4d2337929a9e43d60f
Josh C <jo.118@hotmail.com>
2025-01-23 19:10:17 +0000

    Change to use device sqlite

--- packages/app-mobile/react-native.config.js
+++ packages/app-mobile/react-native.config.js
@@ -11,14 +11,5 @@ module.exports = {
 				ios: null,
 			},
 		},
-		'react-native-sqlite-storage': {
-			platforms: {
-				android: {
-					sourceDir: '../node_modules/react-native-sqlite-storage/platforms/android-native',
-					packageImportPath: 'import io.liteglue.SQLitePluginPackage;',
-					packageInstance: 'new SQLitePluginPackage()',
-				},
-			},
-		},
 	},
 };
