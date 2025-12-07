3408b881d81f235add9d7582db780b53cd6236bc
mrjo118 <jo.118@hotmail.com>
2025-12-07 00:07:29 +0000

    Check without using env vars

--- packages/app-mobile/metro.config.js
+++ packages/app-mobile/metro.config.js
@@ -102,4 +102,11 @@ const config = {
 	watchFolders: watchedFolders,
 };
 
-module.exports = mergeConfig(defaultConfig, getExpoDefaultConfig(__dirname), config);
+// For release builds of the Android app, do not use the expo default config, as this causes an issue with missing icons
+// See https://github.com/laurent22/joplin/issues/13854
+const isAndroid = process.cwd().includes(path.join('packages', 'app-mobile', 'android'));
+const isDev = process.argv.includes('--dev') && process.argv.includes('true');
+
+module.exports = isAndroid && !isDev
+	? mergeConfig(defaultConfig, config)
+	: mergeConfig(defaultConfig, getExpoDefaultConfig(__dirname), config);
