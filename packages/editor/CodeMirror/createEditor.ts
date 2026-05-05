ff4f2bb5a1dfb71a800ca8566d69b37ee05aa888
mrjo118 <jo.118@hotmail.com>
2026-05-06 00:57:15 +0100

    Revert "Add local to remeasure on scroll"
    This reverts commit 2c5124228b42fb33e6aba41f3059debb1bd24898.

--- packages/editor/CodeMirror/createEditor.ts
+++ packages/editor/CodeMirror/createEditor.ts
@@ -117,14 +117,6 @@ const createEditor = (
 		}
 	};
 
-	const notifyViewportRefresh = (viewUpdate: ViewUpdate) => {
-		if (viewUpdate.viewportChanged || viewUpdate.docChanged) return;
-
-		// Force a re-measure cycle on scroll-like updates
-		if (viewUpdate.transactions.some(tr => tr.isUserEvent('scroll'))) {
-			viewUpdate.view.requestMeasure();
-		}
-	};
 
 	const globalSpellcheckEnabled = () => {
 		return editor.contentDOM.spellcheck;
@@ -335,7 +327,6 @@ const createEditor = (
 					notifyDocChanged(viewUpdate);
 					notifySelectionChange(viewUpdate);
 					notifySelectionFormattingChange(viewUpdate);
-					notifyViewportRefresh(viewUpdate);
 				}),
 
 				handleLinkEditRequests(() => {
