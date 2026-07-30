caf2c5a52be43beb407136ecfb764f950da7d5bf
mrjo118 <jo.118@hotmail.com>
2026-07-30 01:53:45 +0100

    Use stopPropagation instead

--- packages/editor/CodeMirror/extensions/rendering/replaceCheckboxes.ts
+++ packages/editor/CodeMirror/extensions/rendering/replaceCheckboxes.ts
@@ -93,8 +93,8 @@ const replaceCheckboxes = [
 		mousedown: (event) => {
 			const target = event.target as Element;
 			if (target.nodeName === 'INPUT' && target.parentElement?.classList?.contains(checkboxClassName)) {
-				// Prevent the keyboard from opening
-				event.preventDefault();
+				// Prevent scrollSelectionIntoView firing in global onclick handler
+				event.stopPropagation();
 				// Let the checkbox handle the event
 				return true;
 			}
