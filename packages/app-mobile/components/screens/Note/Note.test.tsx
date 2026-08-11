b2c37f17a1b2f6c476fe18986bb7b09dc14a34b3
mrjo118 <jo.118@hotmail.com>
2026-08-11 23:26:16 +0100

    Revert "Fix remote overrite while typing for all except very fast typing"
    This reverts commit 7d17eff3ef2b24f7f21ec84335db9335a66738b9.

--- packages/app-mobile/components/screens/Note/Note.test.tsx
+++ packages/app-mobile/components/screens/Note/Note.test.tsx
@@ -221,24 +221,6 @@ describe('screens/Note', () => {
 		unmount();
 	});
 
-	it('should discard a queued save when an editor reload was requested after it was queued', async () => {
-		const noteId = await openNewNote({ title: 'Original title', body: 'Body' });
-		const { unmount } = render(<WrappedNoteScreen />);
-		const titleInput = await screen.findByDisplayValue('Original title');
-
-		await act(async () => {
-			fireEvent.changeText(titleInput, 'Stale local title');
-			const remotelyUpdatedNote = await Note.load(noteId);
-			remotelyUpdatedNote.title = 'Remote title';
-			await Note.save(remotelyUpdatedNote);
-			store.dispatch({ type: 'EDITOR_NOTE_NEEDS_RELOAD', noteId });
-		});
-
-		await waitForNoteToMatch(noteId, { title: 'Remote title' });
-		unmount();
-		await waitForNoteToMatch(noteId, { title: 'Remote title' });
-	});
-
 	it('changing the note body in the editor should update the note\'s body', async () => {
 		const defaultBody = 'Change me!';
 		const noteId = await openNewNote({ title: 'Unchanged title', body: defaultBody });
